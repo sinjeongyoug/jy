c3caf7309a363db23a3a5c1b242d09c806f9d390
tlswjddyd4 <tlswjddyd4@naver.com>
2020-08-18 19:34:05 +0900

    HomeController 워밍업

---
+++ src/main/java/com/sbs/sjy/jy/JyApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.sjy.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyApplication.class, args);
+	}
+
+}
