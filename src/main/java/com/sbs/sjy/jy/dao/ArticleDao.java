f8add420a292ea60588f735818671c85d233d284
tlswjddyd4 <tlswjddyd4@naver.com>
2020-08-19 12:10:40 +0900

    게시물 작성

--- src/main/java/com/sbs/sjy/jy/dao/ArticleDao.java
+++ src/main/java/com/sbs/sjy/jy/dao/ArticleDao.java
@@ -1,6 +1,7 @@
 package com.sbs.sjy.jy.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -12,5 +13,6 @@ public interface ArticleDao {
 	List<Article> getForPrintArticles();
 
 	Article getForPrintArticleById(@Param("id") int id);
-	
+
+	void write(Map<String, Object> param);
 }
