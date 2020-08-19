9a1d87c55d6f899f668d6cdb8186c83521ec2894
tlswjddyd4 <tlswjddyd4@naver.com>
2020-08-19 10:05:43 +0900

    게시물 상세 페이지 구현

--- src/main/java/com/sbs/sjy/jy/dao/ArticleDao.java
+++ src/main/java/com/sbs/sjy/jy/dao/ArticleDao.java
@@ -3,11 +3,14 @@ package com.sbs.sjy.jy.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.sbs.sjy.jy.dto.Article;
 
 @Mapper
 public interface ArticleDao {
 	List<Article> getForPrintArticles();
+
+	Article getForPrintArticles(@Param("id") int id);
 	
 }
