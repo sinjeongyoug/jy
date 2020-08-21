81fae0d76bba4969da523730730fb8fb3ae4a235
tlswjddyd4 <tlswjddyd4@naver.com>
2020-08-21 13:48:36 +0900

    댓글에 동영상 업로드 기능 구현, 업로드된 동영상 스트리밍 기능 구현

--- src/main/java/com/sbs/sjy/jy/dao/FileDao.java
+++ src/main/java/com/sbs/sjy/jy/dao/FileDao.java
@@ -1,6 +1,11 @@
 package com.sbs.sjy.jy.dao;
 
 import java.util.Map;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.sbs.sjy.jy.dto.File;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,4 +14,12 @@ public interface FileDao {
 
 	void save(Map<String, Object> param);
 
+	void changeRelId(@Param("id") int id, @Param("relId") int relId);
+
+	List<File> getFiles(@Param("relTypeCode") String relTypeCode, @Param("relIds") List<Integer> relIds,
+			@Param("typeCode") String typeCode, @Param("type2Code") String type2Code, @Param("fileNo") int fileNo);
+
+	File getFileById(@Param("id") int id);
+
+	void deleteFiles(@Param("relTypeCode") String relTypeCode, @Param("relId") int relId);
 }
